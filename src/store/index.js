13d98167d322835e8eeab12df8d55a7c7667a052
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-08 13:25:04 +0200

    Creamos una funcion  inicial x defecto y se la pasamos al createStore a la vez que el reducers city

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,12 @@
 import { createStore } from "redux";
+import { city } from "./../reducers/city";
 
-export const store = createStore(() => {},
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const inicialState = {
+  city: "Buenos Aires,ar",
+};
+
+export const store = createStore(
+  city,
+  inicialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
