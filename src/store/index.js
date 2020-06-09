66a9f2e317307f89b13785a32eb4073a042ccd8d
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-09 11:12:42 +0200

    Instalamos el Middleware => thunk y cambiamos la configuración de la herramienta de debugger de React Developer Tools para que funcione con el Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+
+import thunk from "redux-thunk";
 import { city } from "./../reducers/city";
 
 const inicialState = {
   city: "Buenos Aires,ar",
 };
 
+//Herramienta debugger React Developer Tools para ver el funcionamiento: (ver documentación), si hay dudas: https://github.com/zalmoxisus/redux-devtools-extension#usage
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//Si no quisieramos utilizar la herramienta simplemente quitamos la constante... Pero es mejor para poder comprobar los estados como cambian.
+//De esta manera ya incorporamos el Middleware => thunk..
 export const store = createStore(
   city,
   inicialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 );
