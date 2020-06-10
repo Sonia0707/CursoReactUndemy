8287ed674b6d7e45695199bf957ca0708b37e615
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-10 12:59:56 +0200

    Cambio en el componente de  clase a componente funcional, mandando el state por el  store

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,88 +1,35 @@
-import React, { Component } from "react";
+import React from "react"; //Borramos  la  conexión del Component ya que ahora no es una clase y utilizamos la conexión a ForecastExtendedContainers
 import PropTypes from "prop-types";
 import ForecastItem from "./ForecastItem";
-import transformForecast from "./../services/transformForecast";
 import "./styles.css";
 
-/*const days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+const renderForecastItemDays = (forecastData) => {
+  return forecastData.map((forecast) => (
+    <ForecastItem
+      key={`${forecast.weekDay}${forecast.hour}`}
+      weekDay={forecast.weekDay}
+      hour={forecast.hour}
+      data={forecast.data}
+    />
+  ));
+};
 
-const data = {
-  temperature: 10,
-  humidity: 10,
-  weatherState: "normal",
-  wind: "normal",
-};*/
-const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
-const url = "https://api.openweathermap.org/data/2.5/forecast";
+//Cambiamos el componente de  clase y lo  convertimos a un funcional component (Componente presentacional), ya que  ahora las peticiones las mandamos por las acciones de redux
+const renderProgress = () => {
+  return "Cargando pronostico estendido...";
+};
 
-class ForecastExtended extends Component {
-  constructor() {
-    super();
-    this.state = { forecastData: null };
-  }
-  componentDidMount() {
-    this.upadateCity(this.props.city);
-  }
+const ForecastExtended = ({ city, forecastData }) => (
+  <div>
+    <h2 className="forcast-title">Pronosticco Extendido para {city}</h2>
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.city !== this.props.city) {
-      this.setState({ forecastData: null });
-      this.upadateCity(nextProps.city);
-    }
-  }
-
-  upadateCity = (city) => {
-    //fetch or axios
-    const url_forecast = `${url}?q=${city}&appid=${api_key}`;
-
-    fetch(url_forecast)
-      .then((data) => data.json())
-      .then((weather_data) => {
-        console.log(`El resultado de weather_data es: ${weather_data}`);
-        const forecastData = transformForecast(weather_data);
-        this.setState({ forecastData });
-      });
-  };
-
-  renderForecastItemDays(forecastData) {
-    return forecastData.map((forecast) => (
-      <ForecastItem
-        key={`${forecast.weekDay}${forecast.hour}`}
-        weekDay={forecast.weekDay}
-        hour={forecast.hour}
-        data={forecast.data}
-      />
-    ));
-  }
-
-  renderProgress = () => {
-    return "Cargando pronostico estendido...";
-  };
-
-  render() {
-    const { city } = this.props;
-    const { forecastData } = this.state;
-    return (
-      <div>
-        <h2 className="forcast-title">Pronosticco Extendido para {city}</h2>
-
-        {forecastData
-          ? this.renderForecastItemDays(forecastData)
-          : this.renderProgress()}
-      </div>
-    );
-  }
-}
+    {forecastData ? renderForecastItemDays(forecastData) : renderProgress()}
+  </div>
+);
 
 ForecastExtended.propTypes = {
   city: PropTypes.string.isRequired,
+  forecastData: PropTypes.array.isRequired,
 };
-
-//Para trabajar la propiedad la propiedad de ciudad en el forcastExtended por defecto
-// se puede hacer de esta manera tambien pero en este curso la tratamos en el App.js:
-
-/*ForecastExtended.defaultProps = {
-  city: "(No hay ciudades por defecto)",
-};*/
 
 export default ForecastExtended;
