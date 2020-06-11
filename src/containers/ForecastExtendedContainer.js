967319dbab4b8ef30879a956b06a7a989b9321f9
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-11 10:35:56 +0200

    Importacion del 1º Selector del reducer city para la controlación del estado desde los reducers y no desde los containers, para no tener que cambiar todos una vez hagamos una modificación.

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { getForecastDataFromCities } from "./../reducers/cities"; //Importamos selector para poder utilizar en el connect
 import ForecastExtended from "../components/ForecastExtended";
 
 //Container que controla los datos  que se le mandan al  Component ForecastExtended
@@ -14,18 +15,28 @@ class ForecastExtendedContainer extends Component {
     );
   }
 }
+//La propiedad forecastData que nos llega no es requerida si o si, porque puede venir undefine ya que en el componente tenemos
+//la ya una respuesta del indicador de progreso: cargando...
+
 ForecastExtendedContainer.propTypes = {
   city: PropTypes.string.isRequired,
-  forecastData: PropTypes.array.isRequired,
+  forecastData: PropTypes.array,
 };
 
 //Objeto de FUNCIONES que se mapea y se inyecta gracias a connect como propiedades dentro del componente:
 // (setea los nuevos valores de propiedades "se ejecuta una renderización")
 
 //Las cities de primeras nos vienen vacias hay que validar y poner algo por defecto
-const mapDispattchToprops = ({ city, cities }) => ({
-  city,
-  forecastData: cities[city] && cities[city].forecastData,
+
+//Hacemos el cambio en este punto para que los containers no controlen el estado porque si no tendríamos que estar cambiando todo el rato los containers
+// y en un proyecto muy grande podría ser un problema... Así que aquí vamos a utilizar el patron selector para hacerlo más eficiente y lo crearemos en los
+// reducers
+
+//Cambiamos el estado a state y llamamos al primer selector, de momento solo tenemos el de city y no el de cities, hay que cambiarlo para que tambien
+//conozca que tenemos un diccionario...
+const mapDispattchToprops = (state) => ({
+  city: state.city,
+  forecastData: getForecastDataFromCities(state.cities, state.city),
 });
 
 export default connect(mapDispattchToprops, null)(ForecastExtendedContainer); // Se suma: MapStateToprops(values) + MapDispattchToprops(func) = propiedades extra para el
