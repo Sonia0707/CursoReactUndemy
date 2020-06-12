dedbe4f3d59ff3472c769cce79035685bdcdd8b8
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-12 11:41:58 +0200

    -En el LocationListContainer:  LLamamos a otra actionCreator para los valores de los climas de las ciudades => setWeather

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { setSelectedCity } from "./../actions";
+import { setSelectedCity, setWeather } from "./../actions";
 import LocationList from "./../components/LocationList";
 
 class LocationListContainer extends Component {
+  //Invocación al ActionCreator de setWeather
+  componentDidMount() {
+    this.props.setWeather(this.props.cities);
+  }
+
   handSelectionLocation = (city) => {
     this.props.setCity(city);
   };
@@ -29,6 +34,10 @@ const mapDispatchTpProps = (dispatch) => ({
   //Nombre de función setCity (Podría ser cualquier nombre no tiene que ser el mismo que el ActionCreator):
   //Cambiamos el nombre del dispatch porque ahora la accion se produce dentro setSelectedCity
   setCity: (value) => dispatch(setSelectedCity(value)), //ActionCreator = setCity
+
+  //Esta acción trae todos los valores de los climas de las ciudades, en este caso el value es el array de ciudades
+  //Se llama al setWeather
+  setWeather: (cities) => dispatch(setWeather(cities)), //ActionCreator = setWeather
 });
 
 export default connect(null, mapDispatchTpProps)(LocationListContainer);
