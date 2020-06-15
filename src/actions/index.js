57675b8e6fbbebff90fb2ea6ab6f2bb6e04574e7
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-15 09:55:57 +0200

    Error solucionado mal escrito type, y Constante SET_WEATHER_CITY..

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,6 @@ import getUrlWeatherByCity from "./../services/getUrlWeatherBtCity";
 //Constantes de las acciones que enviaremos a los reducers para comprobar el state:
 export const SET_CITY = "SET_CITY";
 export const SET_FORECAST_DATA = "SET_FORECAST_DATA";
-export const SET_WEATHER = "SET_WEATHER";
 
 export const GET_WEATHER_CITY = "GET_WEATHER_CITY";
 export const SET_WEATHER_CITY = "SET_WEATHER_CITY";
@@ -18,8 +17,8 @@ const setCity = (payload) => ({ type: "SET_CITY", payload }); //literal correspo
 
 const setForecastData = (payload) => ({ type: SET_FORECAST_DATA, payload }); //Accion que hace la petición al servidor
 
-const getWeatherCity = (payload) => ({ tipe: GET_WEATHER_CITY, payload });
-const setWeatherCity = (payload) => ({ tipe: SET_WEATHER_CITY, payload });
+const getWeatherCity = (payload) => ({ type: GET_WEATHER_CITY, payload });
+const setWeatherCity = (payload) => ({ type: SET_WEATHER_CITY, payload });
 
 const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
 const url = "https://api.openweathermap.org/data/2.5/forecast";
@@ -47,14 +46,13 @@ export const setSelectedCity = (payload) => {
 export const setWeather = (payload) => {
   return (dispatch) => {
     payload.forEach((city) => {
-      console.log(getWeatherCity(city)); // Si que lleva una ciudad la de por defecto Buenos Aires,ar
-      dispatch(getWeatherCity(city)); //Error al hacer el dispatch....???????????????????????????
-      console.log(`Resultado dispatch ${dispatch(getWeatherCity(city))}`);
+      dispatch(getWeatherCity(city));
 
       const api_weather = getUrlWeatherByCity(city);
+
       fetch(api_weather)
-        .then((resolve) => {
-          return resolve.json();
+        .then((data) => {
+          return data.json();
         })
         .then((weather_data) => {
           const weather = transformWeather(weather_data);
