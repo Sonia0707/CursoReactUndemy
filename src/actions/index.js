ff7cc20255ddd23c606ea29e5e7f40643c264dcf
Sonia0707 <vidalgonzalezsonia@gmail.com>
2020-06-16 09:47:16 +0200

    Index.js Actions: Segundo parametro del redux-thunk => (getState). Para que vamos a buscar algo de nuevo si ya tenemos forescast que ya nos hemos traido la primera vez. Con esto evitamos la consulta de nuevo al servidor y hacemos que consulte en el diccionario que ya tenemos creado. Segun fecha y tiempo de consulta si hace menos de un minuto que se consulto la no volver hacer la peticion al servidor.

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,11 +25,21 @@ const url = "https://api.openweathermap.org/data/2.5/forecast";
 
 export const setSelectedCity = (payload) => {
   //Por el Middleware se genera una funcion que tiene que es dispatch
-  return (dispatch) => {
+  return (dispatch, getState) => {
     const url_forecast = `${url}?q=${payload}&appid=${api_key}`;
 
     //Activar en el estado de un indicador de busqueda de datos inicial: (Le ponemos uno por defecto inicial)
     dispatch(setCity(payload)); //literal de la ciudad que el usuario selecciono
+
+    const state = getState();
+    const date =
+      state.cities[payload] && state.cities[payload].forecastDataDate;
+
+    const now = new Date();
+
+    if (date && now - date < 1 * 60 * 1000) {
+      return;
+    }
 
     //Hacemos la promise para buscar los datos de la API y leer el JSON
     return fetch(url_forecast)
